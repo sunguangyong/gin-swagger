6d6b3dee69cb0835919035ada5793d3fb5323062
sunguangyong <5250105+sunguangyong@user.noreply.gitee.com>
2023-07-21 09:21:54 +0800

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -4,17 +4,10 @@ import (
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	"xunjikeji.com.cn/gin-swagger/ginSwagger"
-	//ginSwagger "xunjikeji.com.cn/gin-swagger"
 )
 
 func main() {
-
-	// New gin router
 	router := gin.Default()
-
-	//swag.Register(docs.SwaggerInfov1.InstanceName(), docs.GetSwag(path))
 	router.GET("/swagger/v1/*any", ginSwagger.WrapHandler(swaggerFiles.NewHandler(), ginSwagger.InstanceName("v1")))
-
-	_ = router.Run()
-
+	_ = router.Run(":8989")
 }
